11e20b07175a8190dd8e51c9d328ba04604d5ae9
Cyrus Duong <cyrusduong@gmail.com>
2020-09-12 18:24:41 -0500

    Updates

---
+++ precache-manifest.7058d1b2837ecec1010fc8dd9265bea1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "909949f1cb2dfae90ca29c884f2b8c7d",
+    "url": "/cyrusduong/suru/index.html"
+  },
+  {
+    "revision": "712b2ed6a5e0a8f0c103",
+    "url": "/cyrusduong/suru/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "02cdbcd8e9d3a14440d1",
+    "url": "/cyrusduong/suru/static/css/main.0630a442.chunk.css"
+  },
+  {
+    "revision": "712b2ed6a5e0a8f0c103",
+    "url": "/cyrusduong/suru/static/js/2.fcb44275.chunk.js"
+  },
+  {
+    "revision": "241e2720c0228744c712295a835ee806",
+    "url": "/cyrusduong/suru/static/js/2.fcb44275.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02cdbcd8e9d3a14440d1",
+    "url": "/cyrusduong/suru/static/js/main.8253c495.chunk.js"
+  },
+  {
+    "revision": "5a1242671128d0921969",
+    "url": "/cyrusduong/suru/static/js/runtime-main.eb64952b.js"
+  },
+  {
+    "revision": "5e0d75e0f28eeef6b170d9566ae463ae",
+    "url": "/cyrusduong/suru/static/media/FiraSans-Thin.5e0d75e0.otf"
+  },
+  {
+    "revision": "7a814d9d8801b9532895fd2e468e1a06",
+    "url": "/cyrusduong/suru/static/media/KaushanScript-Regular.7a814d9d.otf"
+  }
+]);
