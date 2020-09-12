88d4b04ab2a450dfec9bbb8be77f309e6cabbdd1
Cyrus Duong <cyrusduong@gmail.com>
2020-09-12 18:15:57 -0500

    docs hack to serve on github pages?

---
+++ docs/precache-manifest.4ebbed5043f392c1c31a979fd767c151.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3939606048f703ffae649acbabe215da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "712b2ed6a5e0a8f0c103",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "29100b1a40e48469a994",
+    "url": "/static/css/main.0c82b4e1.chunk.css"
+  },
+  {
+    "revision": "712b2ed6a5e0a8f0c103",
+    "url": "/static/js/2.fcb44275.chunk.js"
+  },
+  {
+    "revision": "241e2720c0228744c712295a835ee806",
+    "url": "/static/js/2.fcb44275.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29100b1a40e48469a994",
+    "url": "/static/js/main.c3cb559f.chunk.js"
+  },
+  {
+    "revision": "ed6bcb4ab6eb9ebb0c47",
+    "url": "/static/js/runtime-main.36f7972e.js"
+  },
+  {
+    "revision": "5e0d75e0f28eeef6b170d9566ae463ae",
+    "url": "/static/media/FiraSans-Thin.5e0d75e0.otf"
+  },
+  {
+    "revision": "7a814d9d8801b9532895fd2e468e1a06",
+    "url": "/static/media/KaushanScript-Regular.7a814d9d.otf"
+  }
+]);
