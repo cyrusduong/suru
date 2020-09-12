70c1094f6baf36b4de7d6599131804b8b871c6c9
Cyrus Duong <cyrusduong@gmail.com>
2020-09-12 18:02:43 -0500

    Update readme. Add preview image.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ const Header = () => {
   const today = moment().format('MMM Do, YYYY');
 
   return (<div className="header flex-container-center">
-    <h1 className="title">する - To do</h1>
+    <h1 className="title">する (Suru) - To do</h1>
     <h2 className="fira thin">It is: <span className="today-font">{today}</span></h2>
   </div>);
 };
